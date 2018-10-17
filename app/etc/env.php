cef8e1af92a061f0e591a42317adc938bcb375d5
chaos <chaos@iMac-chaos.local>
2018-10-17 23:02:02 +0300

    Initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'chaOS'
+    ],
+    'db' => [
+        'table_prefix' => 'mg_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'xmagento_226',
+                'username' => 'root',
+                'password' => 'chaOS1995',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Wed, 17 Oct 2018 19:06:26 +0000'
+    ]
+];
