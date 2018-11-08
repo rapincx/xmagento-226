3ffc74b22e7caf36af2b593af0f5058038054aab
chaOS <dev.chaosx@gmail.com>
2018-11-08 16:02:00 +0200

    Completed Lesson 3

---
+++ app/code/chaOS/LearnRoute/Controller/JsonResponse/Index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaos
+ * Date: 08.11.18
+ * Time: 15:21
+ */
+
+namespace chaOS\LearnRoute\Controller\JsonResponse;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\Result\Json;
+
+/**
+ * Class Index
+ * @package chaOS\LearnRoute\Controller\JsonResponse
+ */
+class Index extends Action
+{
+    
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     */
+    public function execute()
+    {
+        /**
+         * @var Json $controllerResult
+         */
+        $controllerResult = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $data = ['content' => "https://inchoo.net/magento-2/routing-in-magento-2/"];
+        
+        return $controllerResult->setData($data);
+    }
+}
---
+++ app/code/chaOS/LearnRoute/Controller/ShowPerson/Index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaos
+ * Date: 08.11.18
+ * Time: 15:28
+ */
+
+namespace chaOS\LearnRoute\Controller\ShowPerson;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        $name = 'Yaroslav';
+        $lastName = 'Volovyk';
+        $this->_view->loadLayout()
+            ->getLayout()
+            ->getBlock('learnroute.block')
+            ->setName($name)
+            ->setLastName($lastName);
+        $this->_view->renderLayout();
+    }
+}
